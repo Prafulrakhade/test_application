336d139fdb2514e0162aa9e7c131b0992795e31e
Ubuntu <ubuntu@ip-10-0-0-133.ec2.internal>
2022-12-29 09:13:49 +0000

    update

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.get('/', function (req, res) {
-    res.send('{ "response": "Hello From Techno_1" }');
+    res.send('{ "response": "Hello From Techno_11" }');
 });
 
 app.get('/will', function (req, res) {
